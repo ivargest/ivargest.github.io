db8d29c343f40ebc0413fbe7f730608e935f201d
Ivar Gestoso <ivargest@gmail.com>
2021-02-22 22:22:49 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,47 +1,8 @@
-window.onload = function(){
-    pantalla=document.getElementById("textoPantalla");
+function displayMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
 }
-numeroenpantalla="0";
-numerolisto=0;
-primernumero=0;
-operacion="no";
-      
-function numero(cadena) {
-    if (numeroenpantalla=="0" || numerolisto==0  ) { 
-        pantalla.innerHTML=cadena;
-        numeroenpantalla=cadena;
-    }
-    else {
-        pantalla.innerHTML+=cadena;
-        numeroenpantalla+=cadena;
-    }
-    numerolisto=1
-}
-
-function operar(s) {
-    resultado()
-    primernumero=numeroenpantalla 
-    operacion=s;
-    numerolisto=0;
-}	
-
-function resultado() {
-    if (operacion=="no") {
-        pantalla.innerHTML=numeroenpantalla;	
-    }
-    else {
-        solucionescrita=primernumero+operacion+numeroenpantalla;
-        solucion=eval(solucionescrita)
-        pantalla.innerHTML=solucion
-        numeroenpantalla=solucion;
-        operacion="no";
-        numerolisto=0;
-     }
-}
-
-function borradoTotal() {
-         pantalla.innerHTML=0;
-         numeroenpantalla="0";
-         primernumero=0
-         operacion="no"
-         }
